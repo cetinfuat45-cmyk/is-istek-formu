4bc90ae321142fb4a3038c2963360bb8d575e5dc
cetinfuat45-cmyk <cetinfuat45@gmail.com>
2026-05-06 10:28:14 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'is-istek-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
